218dfb291cb5c52ca9ffe1208bf4f105de4c18ce
Eri Schon <erischon@gmail.com>
2022-05-27 17:26:47 +0200

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import experience from "../data/experience";
 
 const Experience = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <section className="experience" id="Experience">
-      {experience.map((job, index) => (
-        <div key={index}>{job.nom}</div>
-      ))}
+      <div className="experience_container">
+        <div className="container__left">
+          <ul>
+            {experience.map((job, index) => (
+              <div key={index} className="">
+                <li onClick={() => setToggle(!toggle)}>{job.nom}</li>
+                {toggle ? <div className="box">{job.desc}</div> : ""}
+              </div>
+            ))}
+          </ul>
+        </div>
+        {/* <div className="container__right"></div> */}
+      </div>
     </section>
   );
 };
