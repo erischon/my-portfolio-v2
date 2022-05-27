3d355ffb71acf0009d02f7ee4bea277979dee7dc
Eri Schon <erischon@gmail.com>
2022-05-27 08:33:08 +0200

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <img />
+      <nav className="navbar">
+        <ul>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Work</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
