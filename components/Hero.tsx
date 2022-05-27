eac24e27019de47183471df8f3c2373b97bc6a96
Eri Schon <erischon@gmail.com>
2022-05-27 12:50:45 +0200

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,18 @@
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero__title">
-        <h2>Eri Schön</h2>
-        <h1>Je fabrique des apps web</h1>
+    <section className="hero">
+      <div className="hero__container">
+        <div className="hero__title">
+          <h2>Eri Schön</h2>
+          <h1>Je fabrique des apps web</h1>
+        </div>
+        <p>
+          Je suis un Artisan Développeur Full Stack spécialisé dans la
+          construction. En ce moment je suis Freelance.
+        </p>
+        <a href="#">Contactez-moi!</a>
       </div>
-      <p>
-        Je suis un Artisan Développeur Full Stack spécialisé dans la
-        construction. En ce moment je suis Freelance.
-      </p>
-      <button>Contactez-moi!</button>
-    </div>
+    </section>
   );
 };
 
