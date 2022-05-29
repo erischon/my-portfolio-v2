17b62b7e15243b46fbb1ce0a0cf6c4c8c5c90a4e
Eri Schon <erischon@gmail.com>
2022-05-29 11:36:21 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <section className="footer">
+      <span className="footer__copy">
+        &copy; {new Date().getFullYear()} Eri Schön All rights reserved
+      </span>
+    </section>
+  );
+};
+
+export default Footer;
