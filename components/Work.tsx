6368405b527646476b00acb34111032e6a21512a
Eri Schon <erischon@gmail.com>
2022-05-29 10:31:23 +0200

    works section

--- components/Work.tsx
+++ components/Work.tsx
@@ -13,13 +13,35 @@ const Work = ({ works }: any) => {
         <h2>Work</h2>
       </div>
       <div className="works__container">
-        <div className="work__icon">
-          <Image src={iconFolder} alt="" width={30} height={30} />
-          <div>
-            <Image src={iconGit} alt="" width={30} height={30} />
-            <Image src={iconLink} alt="" width={30} height={30} />
+        {works.map((work: any, index: any) => (
+          <div key={index} className="work__card">
+            <div className="work__title">
+              <h3>{work.title}</h3>
+              <div className="work__title-icons">
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <div className="work__icon">
+                    <Image src={iconGit} alt="" width={26} height={26} />
+                  </div>
+                </a>
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <div className="work__icon">
+                    <Image src={iconLink} alt="" width={26} height={26} />
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="work__desc">
+              <p>{work.description}</p>
+            </div>
+            <div className="work__tags">
+              {work.tags.map((tag: any, index: any) => (
+                <div key={index} className="work__tag">
+                  {tag}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
