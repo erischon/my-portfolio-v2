1ee3f8a188e611cf5fbe46e30f55f344c5bb21cd
Eri Schon <erischon@gmail.com>
2022-05-30 09:17:31 +0200

    hero modification

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,9 +3,10 @@ const Hero = () => {
     <section className="hero">
       <div className="hero__container">
         <div className="hero__title">
+          <p className="hero__intro text--secondary">Bonjour, je suis</p>
           <h2>Eri Schön.</h2>
           <h1>Je fabrique des apps web.</h1>
-          <p>
+          <p className="hero__subtitle">
             Je suis un Artisan Développeur Full Stack spécialisé dans la
             construction d&apos;application web. Je suis actuellement Freelance
             Full-Remote.
