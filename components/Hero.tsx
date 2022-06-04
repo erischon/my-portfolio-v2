9f6652068cb7e65e3c476b23180ec910188eb6dd
Eri Schon <erischon@gmail.com>
2022-06-04 12:09:24 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
         <div className="hero__title">
           <p className="hero__intro text--secondary">Bonjour, je suis</p>
           <h2>Eri Schön.</h2>
-          <h1 dangerouslySetInnerHTML={{ __html: copy[0].heroTitle }}>
-            {/* Je <span className="title--underline">confectionne</span> des apps
-            web. */}
+          <h1>
+            Je <span className="title--underline">confectionne</span> des apps
+            web.
           </h1>
           <p className="hero__subtitle">
             Je suis un Artisan Développeur spécialisé dans la confection
