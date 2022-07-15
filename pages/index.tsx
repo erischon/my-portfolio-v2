bfb823d51566e1c6c1d938a154ba61f98216d60b
Eri Schon <erischon@gmail.com>
2022-07-15 08:53:04 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,24 @@ const Home: NextPage = ({ experiences, services, works, seo }: any) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         {/* SEO */}
         <meta name="description" content={seoData.description} />
         <meta name="image" content={seoData.image} />
